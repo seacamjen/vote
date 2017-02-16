907f11698a6fc6a738190646179266e7e7de13e2
cameron jensen and jahan walsh <cameron@>
2017-02-16 08:41:54 -0800

    add jquery voter age confirmation, change class to ID.

--- js/vote.js
+++ js/vote.js
@@ -1,4 +1,10 @@
 $(function(){
   event.preventDefault();
-  
+  var over18 = confirm("Are you over 18? Click ok for yes or Cancel for no.")
+
+  if (over18) {
+    $("#voteAge").show();
+  } else {
+    $("#minors").show();
+  }
 });
